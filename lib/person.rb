796ae085793f35aa6c00d3de20d67c5d9488b048
Joseph Guillory <jagmanx99@yahoo.com>
2022-07-18 21:53:34 -0700

    getting there

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,63 @@
-# your code goes here
+class Person
+    attr_reader :name, :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def bank_account=(balance)
+        @balance = balance
+    end
+
+    def happiness=(level)
+        if level > 10 
+            @happiness = 10
+        elsif level < 0
+            @happiness = 0    
+        else @happiness = level
+        end
+    end
+
+    def hygiene=(cleaned)
+        if cleaned > 10 
+            @hygiene = 10
+        elsif cleaned < 0
+            @hygiene = 0    
+        else @hygiene = cleaned
+        end
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def get_paid amount
+        @bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        '♪ another one bites the dust ♫'
+    end
+
+    def call_friend person
+        self.happiness += 3
+        person.happiness += 3
+        "Hi #{person.name}! It's #{self.name}. How are you?"
+    end
+
+end
